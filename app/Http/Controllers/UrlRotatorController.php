5ead5fe854ce17766728f676d990e1c3c2adf8f0
climax-solution <blocklimax@gmail.com>
2021-08-04 17:43:49 +0900

    create new controller UrlRotatorController

---
+++ app/Http/Controllers/UrlRotatorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UrlRotatorController extends Controller {
+  public function index() {
+
+  }
+
+  public function createNewUrlRotator(Request $request) {
+    
+  }
+}
