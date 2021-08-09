220ea27f6b3d1320d7022c1549063c3f54e77fe6
climax-solution <blocklimax@gmail.com>
2021-08-09 09:17:39 +0900

    create new table KeywordRotator

---
+++ database/migrations/2021_08_06_173514_create_keyword_rotators_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeywordRotatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keyword_rotators', function (Blueprint $table) {
+            $table->id();
+            $table->json('advance_options')->nullable();
+            $table->string('pixel')->nullable();
+            $table->string('amazon_aff_id')->nullable();
+            $table->string('active_rule')->nullable();
+            $table->integer('active_position')->default(0);
+            $table->enum('rotation_option',[0,1,2,3])->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keyword_rotators');
+    }
+}
