90e8a0e7ac7168b911264494a15b859e94a736b5
climax-solution <blocklimax@gmail.com>
2021-08-14 11:09:13 +0900

    check bug geo ip

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -170,7 +170,7 @@ class RedirectController extends Controller
                   case '1':
                     if ( $country_status == false && is_bool($country_status)) $geoip = 1;
                 }
-                // dd($country_status, $row->action);
+                dd($country_status, $row->action);
                 $status[$item] = $geoip;
                 break;
               case '1':
