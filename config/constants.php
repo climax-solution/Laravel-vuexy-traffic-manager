c49e8322a2d0966de84cb348e1f4d6fbe4fafc08
climax-solution <blocklimax@gmail.com>
2021-08-16 11:25:31 +0900

    create new constant file config/constants.php

---
+++ config/constants.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'track' => ['Convomat Default', 'convomat List'],
+    'pixel' => ['Select Pixel', 'Pixel item'],
+    'campaign' => ['Campaign 1', 'Campaign 2'],
+    'countries'  =>  '',
+    'country_group' => [
+      [
+        'group_name' => 'Noth America Main',
+        'list' => 'CA,US,MX'
+      ],
+      [
+        'group_name' => 'North America',
+        'list' => 'AI,AG,AW,BS,BB,BZ,BM,BQ,VG,CA,KY,CR,CU,CW,DM,DO,SV,GL,GD,GP,GT,HT,HN,JM,MQ,MX,PM,MS,CW,KN,NI,PA,PR,BQ,SX,KN,LC,VC,TT,TC,US,VI'
+      ],
+      [
+        'group_name' => 'Europe',
+        'list' => 'EU,AD,AT,BE,BG,CH,CY,CZ,DE,DK,EE,ES,FI,FO,FR,GB,GI,GR,HR,HU,IE,IS,IT,LI,LT,LU,LV,MC,MT,NL,NO,PL,PT,RO,SE,SI,SK,SM,VA,GG,JE'
+      ],
+      [
+        'group_name' => 'Main Europe',
+        'list' => 'EU,AT,BE,DE,DK,ES,FI,FR,GB,GR,IT,LI,LU,NL,PT,SE'
+      ],
+      [
+        'group_name' => 'G7 Group',
+        'list' => 'CA,DE,FR,GB,IT,JP,US'
+      ],
+      [
+        'group_name' => 'Main China',
+        'list' => 'CN,HK,MO,SG,TW'
+      ],
+      [
+        'group_name' => 'Australasia',
+        'list' => 'AU,CC,CX,NA,NF,NZ'
+      ],
+      [
+        'group_name' => 'Main English',
+        'list' => 'AU,CA,GB,IE,MT,NZ,SG,US,GG,JE'
+      ],
+      [
+        'group_name' => 'South America',
+        'list' => 'AR,BO,BR,CL,CO,EC,FK,GF,GY,GY,PY,PE,SR,UY,VE'
+      ],
+      [
+        'group_name' => 'Russian Language',
+        'list' => 'BY,RU,UA'
+      ],
+      [
+        'group_name' => 'Ex USSR',
+        'list' => 'AM,AZ,BY,EE,GE,KG,KZ,LT,LV,MD,RU,TJ,TM,UA,UZ'
+      ]
+    ]
+  ];
+?>
